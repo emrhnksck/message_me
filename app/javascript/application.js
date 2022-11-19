d1292902b0a4811405f3c9049b615033d5732ed6
Emrehan <emrehan.kisacik@hotmail.com>
2022-11-19 22:11:50 +0300

    Add controllers and header

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-//= require semantic-ui
-//= require require jquery
+import "jquery"
+import "semantic-ui"
+
+$(document).on('turbo:load',function() {
+    $('.ui.dropdown').dropdown();
+})
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
-  get 'sessions/index'
+  get 'login', to: 'sessions#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
