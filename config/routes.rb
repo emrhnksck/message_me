d90a6fd4c03f6b41247b286452f714e1bc0780ca
Emrehan <emrehan.kisacik@hotmail.com>
2022-11-20 21:51:29 +0300

    Add send chat from textfield ability

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  post 'message', to: 'messages#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
